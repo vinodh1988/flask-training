ee9be10fe06e400eeed0142321411f3c1ec88f82
vinodh1988 <k.c.vinodh@hotmail.com>
2022-11-28 12:48:05 +0530

    post

--- routes/dbroutes.py
+++ routes/dbroutes.py
@@ -1,9 +1,22 @@
 from config import app,db
 from database import Person
-from flask import jsonify
+from flask import jsonify,request,abort
 
 @app.route('/people')
 def getPeople():
     listp=Person.query.all()
     result=[ x.serialize() for x in listp]
     return jsonify(result)
+
+@app.route('/people',methods=['post'])
+def addPeople():
+    try:
+        input=request.get_json()
+        sno=input['sno']
+        name=input['name']
+        city=input['city']
+        db.session.add(Person(sno,name,city))
+        db.session.commit()
+        return {"status":"success"}, 201
+    except:
+        abort ( {"status": "Server error"}, 500)
