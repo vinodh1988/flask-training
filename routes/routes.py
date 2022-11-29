ccd28fab959371f571ffc8368d8b3ed2dbf4ecf7
vinodh1988 <k.c.vinodh@hotmail.com>
2022-11-29 10:45:00 +0530

    new-route

--- routes/routes.py
+++ routes/routes.py
@@ -1,10 +1,10 @@
 from config import app
 from flask import render_template
-@app.get('/')
+@app.get('/home')
 def home():
     return render_template('index.html', programmer="Joseph")
 
 @app.get("/greet/<string:name>")
 def greet(name):
-    return "Hi!!"+name
+    return render_template('greet.html',name=name)
     
