d341d88ae29b4e7f18457082ac7b4243e5aa444e
vinodh1988 <k.c.vinodh@hotmail.com>
2022-11-29 10:05:16 +0530

    static-files

--- config/__init__.py
+++ config/__init__.py
@@ -1,4 +1,4 @@
-from flask import Flask
+from flask import Flask,send_from_directory
 import os
 from flask_sqlalchemy import SQLAlchemy
 
@@ -6,6 +6,10 @@ basedir=os.path.abspath(os.path.dirname(__file__))
 
 print(basedir)
 app=Flask(__name__)
+
+@app.route("/files/<path:path>")
+def get_file(path):
+    return send_from_directory('../static',path)
 
 app.config['SQLALCHEMY_DATABASE_URI']="sqlite:///"+os.path.join(basedir,'mydb.db')
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS']= False
